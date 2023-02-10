ef00e35384858fa653674289510ee61b4305a1d6
seif <116313275+sahnoune1@users.noreply.github.com>
2023-02-10 20:10:22 +0100

    dist/index.html

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-alert("hey fuck you!");
+(() => {
+  "use strict";
+  const e = document.getElementById("content");
+  !(function (e, t, n) {
+    const i = document.createElement("div");
+    i.setAttribute("class", "header newClass"),
+      (i.innerHTML = "fuck i hope this work"),
+      e.appendChild(i);
+  })(e),
+    (function (t) {
+      const n = document.createElement("img");
+      n.setAttribute("src", "../piza.jpg"), e.appendChild(n);
+    })(),
+    (function (t, n) {
+      const i = document.createElement("div");
+      i.setAttribute("class", "seif"),
+        (i.innerHTML = "i hope this works too?"),
+        e.appendChild(i);
+    })();
+})();
